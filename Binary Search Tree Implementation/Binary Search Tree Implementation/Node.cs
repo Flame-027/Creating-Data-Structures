0176e170276d16831be7da27a7fa8aab2efed8ad
Husnain Majeed <husnainm3@gmail.com>
2022-12-29 18:01:19 +0000

    Started work on Binary Search Tree

---
+++ Binary Search Tree Implementation/Binary Search Tree Implementation/Node.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Binary_Search_Tree_Implementation
+{
+    public class Node
+    {
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+        public int Value { get; set; }
+
+        public Node() { }
+
+        public Node(int value)
+        {
+            this.Value = value;
+        }
+
+    }
+}

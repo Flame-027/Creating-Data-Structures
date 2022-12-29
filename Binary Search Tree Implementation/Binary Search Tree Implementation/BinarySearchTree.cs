13a2708e4e9d047207aa3bcc94d3358963725e4e
Husnain Majeed <husnainm3@gmail.com>
2022-12-29 18:02:11 +0000

    Created Binary Search Tree Insert method

--- Binary Search Tree Implementation/Binary Search Tree Implementation/BinarySearchTree.cs
+++ Binary Search Tree Implementation/Binary Search Tree Implementation/BinarySearchTree.cs
@@ -13,7 +13,35 @@ namespace Binary_Search_Tree_Implementation
 
         public BinarySearchTree() { }
 
-       
+        public BinarySearchTree(int value)
+        {
+            Insert(value);
+        }
+
+        public void Insert(int value)
+        {
+            if (_size == 0)
+                _root.Value = value;
+            else
+            {
+                var current = _root;
+                bool completed = false;
+
+                while (!completed)
+                {
+                    if (current == null)
+                    {
+                        current = new Node(value);
+                        completed = true;
+                    }
+                    if (value < current.Value)
+                        current = current.Left;
+                    else
+                        current = current.Right;
+                }
+                _size++;
+            }
+
         }
     }
 }

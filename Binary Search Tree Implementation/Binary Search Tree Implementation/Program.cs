4c058cc63abc5e4bf6a759af7e028b69a5f9a2f0
Husnain Majeed <husnainm3@gmail.com>
2022-12-30 14:53:28 +0000

    Ran some tests

--- Binary Search Tree Implementation/Binary Search Tree Implementation/Program.cs
+++ Binary Search Tree Implementation/Binary Search Tree Implementation/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Binary_Search_Tree_Implementation;
+
+class Program
+{
+    static void Main()
+    {
+        var tree = new BinarySearchTree();
+
+        tree.Insert(100);
+        tree.Insert(55);
+        tree.Insert(40);
+        tree.Insert(50);
+        tree.Insert(35);
+        tree.Insert(18);
+    }
+}
